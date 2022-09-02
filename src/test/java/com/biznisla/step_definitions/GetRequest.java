ae8ae6fa8a6a24de35662c6e48c54f7e1149399a
Fatma Guleryuz <fglryz.111@gmail.com>
2022-09-02 16:07:28 +0100

    retrieve feature is done

--- src/test/java/com/biznisla/step_definitions/GetRequest.java
+++ src/test/java/com/biznisla/step_definitions/GetRequest.java
@@ -2,38 +2,70 @@ package com.biznisla.step_definitions;
 
 import com.biznisla.pages.BasePage;
 import com.biznisla.pages.TestBase;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.poi.ss.formula.functions.T;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import java.util.Optional;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GetRequest  {
     Response response;
     String baseurl="http://54.91.11.180:8000";
 
-    @When("User get the  spartans from api")
-    public void userGetTheSpartansFromApi() {
-        Response response  = given().accept(ContentType.JSON)
-                .when()
-                .get(baseurl + "/api/spartans");
+
+
+
+    @When("API get request is made to endpoint {string}")
+    public void apiGetRequestIsMadeToEndpoint(String endpoint) {
+        response=given().accept(ContentType.JSON).when()
+                .get(endpoint);
     }
 
+    @Then("verify status code is {int}")
+    public void verify_status_code_is(Integer statusCode) {
+        assertEquals(statusCode,response.getStatusCode());
+
+    }
+
+    @And("Verify response content type is {string}")
+    public void verifyResponseContentTypeIs(String contentType) {
+        assertTrue(response.getContentType().contains(contentType));
+    }
+
+    @When("print out the response  body to the console")
+    public void printOutTheResponseBodyToTheConsole() {
+        response.prettyPrint();
+    }
+    @When("Print out the user's id")
+    public void print_out_the_user_s_id() {
+   int id=response.path("[1].id");
+        System.out.println("id = " + id);
+
+    }
+    @When("Print out the user's name")
+    public void print_out_the_user_s_name() {
+        String name=response.path("[1].name");
+        System.out.println("name = " + name);
 
 
-        @Then("status code should be {int}")
-        public void status_code_should_be(Integer statusCode) {
-            assertEquals(response.statusCode(),200);
-        }
+    }
+    @When("Print out the user's gender")
+    public void print_out_the_user_s_gender() {
+        String gender=response.path("[1].gender");
+        System.out.println("gender = " + gender);
 
-
+    }
 
 }
--- src/test/resources/features/allSpartans.feature
+++ src/test/resources/features/allSpartans.feature
@@ -1,6 +1,18 @@
-Feature: To test get all spartans
-
 @wip
-Scenario: Get All Spartans
-  When User get the  spartans from api
-  Then status code should be 200
+Feature:Verify all spartans
+  Scenario: Retrieve all spartans via API
+    When API get request is made to endpoint "http://54.91.11.180:8000/api/spartans"
+    Then verify status code is 200
+    And Verify response content type is "application/json"
+    When print out the response  body to the console
+    And Print out the user's id
+    And Print out the user's name
+    And Print out the user's gender
+
+    Scenario
+
+
+
+
+
+
